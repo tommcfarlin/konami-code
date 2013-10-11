266b67f8bf1f24b8715341eb6151f809cbf0c749
Tom McFarlin <tom@tommcfarlin.com>
2013-10-11 08:30:16 -0400

    moving the konami plugin and minified code to a `src` directory

--- src/jquery.konami.js
+++ src/jquery.konami.js
@@ -1,24 +1,26 @@
 /*
  * Konami Code For jQuery Plugin
+ * 1.2.0, 11 October 2013
  *
  * Using the Konami code, easily configure and Easter Egg for your page or any element on the page.
  *
- * Copyright 2011 - 2013 8BIT, http://8BIT.io
+ * Copyright 2011 - 2013 Tom McFarlin, http://tommcfarlin.com
  * Released under the MIT License
  */
 
 (function ( $ ) {
 	"use strict";
-	
-	$.fn.konami = function( options ) {
-		
-		var opts, masterKey, controllerCode, code;
 
-		var opts = $.extend({}, $.fn.konami.defaults, options);
+	$.fn.konami = function( options ) {
+
+		var opts, masterKey, controllerCode, code;
+		opts = $.extend({}, $.fn.konami.defaults, options);
+
 		return this.each(function() {
-		
+
 			masterKey = [38,38,40,40,37,39,37,39,66,65];
 			controllerCode = [];
+
 			$( window ).keyup(function( evt ) {
 
 				code = evt.keyCode || evt.which;
@@ -26,25 +28,25 @@
 				if ( 10 > controllerCode.push( code ) ) {
 					return;
 				} // end if
-				
+
 				if ( 10 < controllerCode.length ) {
 					controllerCode.shift();
 				} // end if
-				
+
 				if ( masterKey.toString() !== controllerCode.toString() ) {
 					return;
 				} // end for
-				
+
 				opts.cheat();
-				
-			}); // keyup
-			
-		}); // each
-		
-	}; // opts
-	
+
+			}); // end keyup
+
+		}); // end each
+
+	}; // end opts
+
 	$.fn.konami.defaults = {
 		cheat: null
 	};
-	
-}(jQuery))
+
+}(jQuery));
